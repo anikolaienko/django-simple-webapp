2fed9619565b588259c91cbefce1703fc174f781
Andrew Nikolayenko <nikolayenko.andrew@gmail.com>
2020-08-18 17:23:02 +0400

    initialization

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Django Simple WebApp
+
+Simplistic web application written in Python3 using Django framework.
+
+I have spent quite some time learning and practicing Algorithms, Data Structures, System Design,Distributed Systems, Operating Systems, preparing to FAANG interviews. This is my come back to web development.
+
+If you are interested in my profile, find more information about me on [andnik.com](http://andnik.com)
---
+++ polls/models.py
@@ -0,0 +1,28 @@
+from datetime import timedelta
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        """
+        True if question was publication date is not older than 24 hours from now
+        and not in the future.
+        """
+        now = timezone.now()
+        return now - timedelta(days=1) <= self.pub_date < now
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
